1a743e9eec73db776324a05a05f1457075c977e8
iguezees <106547871+fresaip@users.noreply.github.com>
2023-06-23 16:31:26 +0100

    Update draw_engine.py

--- customtkinter/draw_engine.py
+++ customtkinter/draw_engine.py
@@ -3,7 +3,7 @@ import sys
 import math
 import tkinter
 from typing import Union, TYPE_CHECKING
-
+=======================================
 if TYPE_CHECKING:
     from .widgets.ctk_canvas import CTkCanvas
 
